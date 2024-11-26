75a2340cb35f8b233f155e9705bf7714a43252c2
Syed Mohiuddin <mohiuddinrx6@gmail.com>
2024-11-27 02:11:44 +0500

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,99 +1,24 @@
 module.exports = {
   env: {
-    browser: true,
-    es2021: true,
+    browser: true, // Allows for browser global variables like `window` and `document`
+    es2021: true,  // Supports ES2021 syntax
   },
   extends: [
-    'plugin:react/recommended',
-    'airbnb',
+    'eslint:recommended',       // Base ESLint recommended rules
+    'plugin:react/recommended', // React-specific linting rules
   ],
   parserOptions: {
     ecmaFeatures: {
-      jsx: true,
+      jsx: true, // Allows for parsing JSX
     },
-    ecmaVersion: 12,
-    sourceType: 'module',
+    ecmaVersion: 12, // Supports modern ECMAScript features
+    sourceType: 'module', // Allows for ES Modules
   },
   plugins: [
-    'react',
+    'react', // React-specific linting plugin
   ],
   rules: {
-    'react/no-unescaped-entities': 0,
-    'eslintreact/no-danger': 0,
-    'react/jsx-max-props-per-line': 0,
-    'react/jsx-first-prop-new-line': 0,
-    'no-console': 0,
-    'jsx-a11y/label-has-associated-control': 0,
-    'no-nested-ternary': 0,
-    'consistent-return': 0,
-    'no-alert': 0,
-    'react/jsx-no-constructed-context-values': 0,
-    'import/extensions': 0,
-    'react/prop-types': 0,
-    'linebreak-style': 0,
-    'react/state-in-constructor': 0,
-    'import/prefer-default-export': 0,
-    'react/react-in-jsx-scope': 'off',
-    'react/jsx-props-no-spreading': 'off',
-    'jsx-a11y/no-noninteractive-element-interactions': 'off',
-    'react/function-component-definition': [
-      2,
-      {
-        namedComponents: 'arrow-function',
-        unnamedComponents: 'arrow-function',
-      },
-    ],
-    'max-len': [
-      2,
-      1050,
-    ],
-    'no-multiple-empty-lines': [
-      'error',
-      {
-        max: 1,
-        maxEOF: 1,
-      },
-    ],
-    'no-underscore-dangle': [
-      'error',
-      {
-        allow: [
-          '_d',
-          '_dh',
-          '_h',
-          '_id',
-          '_m',
-          '_n',
-          '_t',
-          '_text',
-        ],
-      },
-    ],
-    'object-curly-newline': 0,
-    'react/jsx-filename-extension': 0,
-    'react/jsx-one-expression-per-line': 0,
-    'jsx-a11y/click-events-have-key-events': 0,
-    'jsx-a11y/alt-text': 0,
-    'jsx-a11y/no-autofocus': 0,
-    'jsx-a11y/no-static-element-interactions': 0,
-    'react/no-array-index-key': 0,
-    'jsx-a11y/anchor-is-valid': [
-      'error',
-      {
-        components: [
-          'Link',
-        ],
-        specialLink: [
-          'to',
-          'hrefLeft',
-          'hrefRight',
-        ],
-        aspects: [
-          'noHref',
-          'invalidHref',
-          'preferButton',
-        ],
-      },
-    ],
+    // Use recommended rules; add custom rules here if needed in the future
   },
 };
+
